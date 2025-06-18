e0801bc16abe955dfa484a909be69a506e9b8083
Ansh Gupta <anshgupta1161@gmail.com>
2025-06-18 11:55:10 +0530

    first-commit

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { protectedRoute } from "../middleware/auth.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
+
+const messageRouter = express.Router();
+
+messageRouter.get("/users",protectedRoute,getUsersForSidebar)
+messageRouter.get("/:id",protectedRoute,getMessages)
+messageRouter.put("/mark/:id",protectedRoute,markMessageAsSeen)
+messageRouter.post("/send/:id",protectedRoute,sendMessage)
+
+export default messageRouter;
